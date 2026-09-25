aa2e261b9dda804aebf0d607ee560ff0d994087c
Denis_Ruiz <denisruime.20@gmail.com>
2026-09-25 18:35:28 -0500

    fix: elimine codigo que no hacia nada en el index.tsx del grupo tabs

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,33 +1,9 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/themed-text';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-import { View } from 'react-native';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-
+      {/* Aquí irá el contenido de tu pantalla de Inicio */}
     </View>
   );
 }
@@ -35,36 +11,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#FFFCF2',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+    backgroundColor: "#FFFCF2", // Mantengo tu color de fondo
   },
 });
